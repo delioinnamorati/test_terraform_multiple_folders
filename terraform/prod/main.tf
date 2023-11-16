c34a399d6332711a6d67070788920f857394eb77
Delio <>
2023-11-16 11:07:40 +0000

    add GH actions

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -3,28 +3,36 @@ terraform {
 }
 
 terraform {
-  required_providers {
+    required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~>5.25.0"
     }
   }
 }
 provider "aws" {
-    region = "us-east-1"
-    #assume_role {
-        #role_arn = "arn:aws:iam::${var.account_id}:role/${var.role}"
-    #}
+  region =  "us-east-1"
+  #assume_role {
+  #role_arn = "arn:aws:iam::${var.account_id}:role/${var.role}"
+  #}
 }
 terraform {
   backend "s3" {
     dynamodb_table = "terraform_state"
-    key = "terraform.tfstate"
-    bucket = "test123-example1-app1"
-    region="us-east-1"
-    encrypt=true
+    key            = "terraform.tfstate"
+    bucket         = "test123-example1-app1"
+    region         = "us-east-1"
+    encrypt        = true
   }
 }
 resource "aws_vpc" "main" {
   cidr_block = "10.2.0.0/16"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.2.0.0/16"
+}
+resource "aws_instance" "foo" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t1.2xlarge" # invalid type!
+}
+
