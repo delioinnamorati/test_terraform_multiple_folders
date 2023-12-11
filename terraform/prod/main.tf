9079d4d7db5102b6884d1dca26151ed4776217e9
Delio <>
2023-12-11 15:45:39 +0000

    test

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -35,4 +35,8 @@ resource "aws_instance" "foo" {
   ami           = "ami-0ff8a91507f77f867"
   instance_type = "t1.2xlarge" # invalid type!
 }
+resource "aws_instance" "deb" {
+  ami           = "ami-0ff8a91507f77f867"
+  instance_type = "t1.2xlarge" # invalid type!
+}
 # trigger change
